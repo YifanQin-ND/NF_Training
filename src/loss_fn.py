bdf0d1c79fb4589edc9476bb53a8a9e0842291c8
yifanqin <yqin3@nd.edu>
2024-01-09 20:45:21 -0500

    update

--- src/loss_fn.py
+++ src/loss_fn.py
@@ -1,4 +1,4 @@
-def negative_feedback(output_backbone, label, beta, criterion, *out):
-    nf_out = output_backbone - beta * sum((10**(-i)) * output for i, output in enumerate(out))
-    loss = criterion(nf_out / (len(out)+1), label)
+def negative_feedback(output_backbone, label, beta, criterion, outs):
+    nf_out = output_backbone - beta * sum((10**(-i)) * output for i, output in enumerate(outs))
+    loss = criterion(nf_out / (len(outs)+1), label)
     return loss
